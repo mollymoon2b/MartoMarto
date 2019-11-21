d3a153ac185db35091ac935a1b17e3ee277632e5
Pierre TROLLÉ <pierre.trolle@manomano.com>
2019-11-21 17:12:16 +0100

    chore(clean): rename folder

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,8 +2,8 @@ import 'phaser';
 import hammer from '../assets/hammer.png';
 import nail from '../assets/nail.png';
 import { getHammerPosition } from '../utils/getHammerPosition';
-import { catchHammer } from '../utils/hammer/catchHammer';
-import { handleDraggingHammer } from '../utils/hammer/handleDraggingHammer';
+import { catchHammer } from '../utils/hammer-input/catchHammer';
+import { handleDraggingHammer } from '../utils/hammer-input/handleDraggingHammer';
 import { hammeringNail } from '../utils/nail/hammeringNail';
 
 export default class GameScene extends Phaser.Scene {
