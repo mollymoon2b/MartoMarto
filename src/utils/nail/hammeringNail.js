dfe89e6309c3bd0bbdc97d5f69dd9aaac7733f93
Eliam BOUMERZOUG <eliam.boumerzoug@manomano.com>
2019-11-21 18:03:41 +0100

    compute velocity hammering

--- src/utils/nail/hammeringNail.js
+++ src/utils/nail/hammeringNail.js
@@ -1,8 +1,15 @@
 let nailOrigin = { x: 0.5, y: 0.5 };
+const velocity = 0.10;
 
-const hammeringNail = (nail,velocity) => {
+const computeVelocity = (percentage) => {
 
+  const toto = percentage > 0 ? percentage * velocity : 0;
+  console.log({velocity})
+  return toto;
+}
 
+const hammeringNail = (nail, percentage) => {
+  console.log('y',nailOrigin.y)
     if(nailOrigin.y === 0.15) {
       // win
     }
@@ -10,7 +17,7 @@ const hammeringNail = (nail,velocity) => {
     if(nailOrigin.y < 0.15) {
       // Lose
     } else {
-      nailOrigin.y -= velocity;
+      nailOrigin.y -= computeVelocity(percentage);
       nailOrigin.y = Math.round(nailOrigin.y * 100) / 100;
       console.log(nailOrigin.y)
       nail.setOrigin(nailOrigin.x, nailOrigin.y);
