bb31321ba1bb7cc27ec7104bed1a7813f77629c7
Pierre TROLLÉ <pierre.trolle@manomano.com>
2019-12-05 16:08:45 +0100

    feat(table): animate faster table

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,8 +1,8 @@
 import 'phaser';
 import hammer from '../assets/hammer.png';
 import nail from '../assets/nail.png';
-import tableSpritesheet from '../assets/spritesheet.png';
-import tableSpritesheetJson from '../assets/spritesheet.json';
+import tableSpritesheet from '../assets/table.png';
+import tableSpritesheetJson from '../assets/table.json';
 import { getHammerPosition } from '../utils/getHammerPosition';
 import { catchHammer } from '../utils/hammer-input/catchHammer';
 import { handleDraggingHammer } from '../utils/hammer-input/handleDraggingHammer';
@@ -73,44 +73,23 @@ export default class GameScene extends Phaser.Scene {
     const configBreakTable = {
       key: 'breakTable',
       frames: [
-        { key: 'tableSpritesheet', frame: 'annimation-table0001.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0002.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0003.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0004.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0005.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0006.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0007.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0008.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0009.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0010.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0011.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0012.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0013.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0014.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0015.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0016.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0017.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0018.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0019.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0020.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0021.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0022.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0023.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0024.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0025.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0026.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0027.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0028.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0029.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0030.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0031.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0032.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0033.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0034.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0035.png' },
-        { key: 'tableSpritesheet', frame: 'annimation-table0036.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0001.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0002.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0003.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0004.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0005.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0006.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0007.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0008.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0009.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0010.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0011.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0012.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0013.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0014.png' },
+        { key: 'tableSpritesheet', frame: 'final-animation0015.png' },
       ],
-      frameRate: 100, // frame rate – the speed of the animation. Higher is faster.
+      frameRate: 60, // frame rate – the speed of the animation. Higher is faster.
     };
 
     this.anims.create(configBreakTable);
