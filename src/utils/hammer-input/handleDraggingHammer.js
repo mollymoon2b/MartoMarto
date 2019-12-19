b4311b9bd8e761457a3eeba2922fa512c4a54d81
Josselin Buils <josselin.buils@manomano.com>
2019-12-19 16:06:59 +0100

    fix(hammer): fixes hammer blocked on top position

--- src/utils/hammer-input/handleDraggingHammer.js
+++ src/utils/hammer-input/handleDraggingHammer.js
@@ -31,7 +31,6 @@ const handleDraggingHammer = (hammer, game, moveHammer, animateHammer, hammering
       const newPosition = getPosition(gameObject);
       const percentage = calculatePercentageHammer({ originPosition, newPosition });
       animateHammer(percentage);
-      preventDragEnd = true
     } else {
       preventDragEnd = false
     }
